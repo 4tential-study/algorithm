9905e69cf8366cfc020409e08ebe38e89a90033c
seoeunbae <enyoupark@gmail.com>
2023-07-21 06:39:41 +0900

    code(진행중) : [Baekjoon]15686-배열로 변경

--- baekjoon/boj_15686.java
+++ baekjoon/boj_15686.java
@@ -7,11 +7,6 @@ import java.util.StringTokenizer;
 
 public class boj_15686 {
     static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-    static HashMap<Integer, Home> homes = new HashMap<>();
-    static ArrayList<Home> homeArrays = new ArrayList<>();
-    static ArrayList<Chicken> chickenArrays = new ArrayList<>();
-
-    static HashMap<Integer, Chicken> chickens = new HashMap<>();
 
     public static void main(String[] args) throws IOException {
         String[] NM = br.readLine().split(" ");
@@ -19,6 +14,8 @@ public class boj_15686 {
         int M = Integer.parseInt(NM[1]);
 
         int[][] board = new int[N][N];
+        ArrayList<int[]> chickens = new ArrayList<>();
+        ArrayList<int[] > homes = new ArrayList<>();
         int chickenCount = 0;
         int homeCount = 0;
         for(int i = 0 ; i < N ; i++) {
@@ -26,72 +23,53 @@ public class boj_15686 {
             for(int j =0 ; j < N ; j++ ) {
                 board[i][j] = Integer.parseInt(st.nextToken());
                 if (board[i][j] == 1){
-
-                    Home home = new Home(i,j);
-                    homes.put(++homeCount,home);
+                    homes.add(new int[]{i,j});
                 }
                 if (board[i][j] == 2){
-//                    chickenCount++;
-                    Chicken chicken = new Chicken(i,j);
-                    chickens.put(++chickenCount, chicken);
+                    chickens.add(new int[]{i,j});
                 }
             }
         }
 
-        for(Home home : homeArrays){
-            int x = home.x;
-            int y = home.y;
-            //집마다 치킨거리 구하기
-            int minDist = 10000;
-            for(Chicken chicken : chickenArrays){
-                int x1 = chicken.x;
-                int y1 = chicken.y;
-                int distance = calculateDistance(x, y, x1, y1);
-                if (minDist > distance) {
-                    minDist = distance;
-                }
-            }
-            // 치킨거리 저장
-            home.chickenDistance = minDist;
-        }
+        search( homes, chickens, M)
 
-
-
-
-
-    }
-
-    public static class Home{
-        int y;
-        int x;
-        int chickenDistance;
-
-        Home(int y, int x){
-            this.y=y;
-            this.x=x;
-        }
-    }
-
-    public static class Chicken{
-        int y;
-        int x;
-        Chicken(int y, int x){
-            this.y=y;
-            this.x=x;
-        }
+//        System.out.println(answer);
     }
 
     private static int calculateDistance(int x, int y, int x1, int y1){
         return Math.abs(x-x1) + Math.abs(y- y1);
     }
 
-    public static void backtracking(int x, int y, int m, ArrayList<int[]> mlist){
-        if (mlist.size() == m){
+    public static void search( ArrayList<int[]> homes, ArrayList<int[]> chickens , int m){
+        int result = 0;
+        int answer = 0;
 
-            return;
+        for(int[] home : homes){
+            int x = home[0];
+            int y = home[1];
+            //집마다 치킨거리 구하기
+            int minDist = 10000;
+            for(int[] chicken : chickens){
+//                if (cur == m) {
+//                    break;
+//                }
+                int x1 = chicken[0];
+                int y1 = chicken[1];
+
+                int distance = calculateDistance(x, y, x1, y1);
+                System.out.println("distance"+distance);
+                if (minDist > distance) {
+                    minDist = distance;
+                }
+
+                System.out.println(": "+ minDist); //
+
+            }
+            result+=minDist;
+            System.out.println(result);
         }
 
-
     }
+
 
 }

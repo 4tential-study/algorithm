e20a982baf4396dcd8e2fcd4dfd84abd76d137ef
jieonkim23 <kwldjs1023@gmail.com>
2023-07-24 01:40:54 +0900

    [Baekjoon]2436 - 완전탐색

--- baekjoon/2436.cpp
+++ baekjoon/2436.cpp
@@ -3,27 +3,22 @@
 using namespace std;
 int Gcd, Lcm;
 
-// int findGcd(int a, int b) {  //구글링함ㅎ
-//     if(b==0) return a;
-//     return findGcd(b, a%b);
-// }
+int findGcd(int a, int b) {  //구글링함ㅎ
+    if(b==0) return a;
+    return findGcd(b, a%b);
+}
 
 int main() {
     cin >> Gcd >> Lcm;
     long long num1 = Gcd, num2 = Lcm;
-    for(int i=1; i < Lcm/Gcd; i++){
-
-        for(int j=i+1; j<Lcm/Gcd; j++){
-            long long a = Gcd*i;
-            long long b = Gcd*j;
-            if(a * b / Gcd == Lcm) {
-                if(num1 + num2 > a + b) {
-                    num1 = a;
-                    num2 = b;
-                }
+    for(int i=1; i <= sqrt(Lcm/Gcd); i++){
+        if((Lcm/Gcd) % i == 0){
+            if(findGcd(i, (Lcm/Gcd) / i) == 1) {
+                num1 = i*Gcd;
+                num2 = (Lcm/Gcd) / i * Gcd;
             }
         }
-    }
+     }
 
     cout << num1 << ' ' << num2;
 }

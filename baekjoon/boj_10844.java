9aed2a8fbc2bc175a2ec38c5d07033d97e2ad61e
seoeunbae <enyoupark@gmail.com>
2023-08-01 21:03:55 +0900

    [Baekjoon] 10844.쉬운계단수 - dp

--- baekjoon/boj_10844.java
+++ baekjoon/boj_10844.java
@@ -4,42 +4,39 @@ import java.io.InputStreamReader;
 
 public class boj_10844 {
 	static int N ;
-	static int[][] dp;
-	static boolean[] visited;
-	static int count ;
+	static final long mod = 1000000000;
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		N = Integer.parseInt(br.readLine());
 		
-		dp = new int[N+1][10];
-		//골라야하는 개수
+		long[][] dp = new long[N+1][10];
 		
-		for(int i=0 ; i < 10; i++) {
-			dp[0][i] = 0;
-			dp[1][i]= 1;
+		for(int i=1 ; i < 10; i++) {
+			dp[1][i] = 1;
 		}
-		
-		for(int i=2 ; i <= N  ; i++) {
-			for(int j = 0 ; j < 10 ; j++) {
-				if (j-i < 0 || j+i-1 > 9 ) {
-					System.out.println("if"+j);
-					dp[i][j] = 2* (dp[i-1][j]) - 1;
-					System.out.println(dp[i][j]);
-				} else {
-					System.out.println("else");
-					dp[i][j] = 2* (dp[i-1][j]);
-					System.out.println(dp[i][j]);
+
+		for(int i=2; i<=N; i++) {
+			/* 현재 자릿값을 0부터 9까지 탐색*/
+			for(int j=0; j<10; j++) {
+				// 자릿값이 9라면 이전 자릿값은 8만 가능
+				if(j == 9) {
+					dp[i][9] = dp[i-1][8]%mod;
 				}
-				
+				// 자릿값이 0이라면 이전 자릿값은 1만 가능
+				else if(j==0) {
+					dp[i][0] = dp[i-1][1] % mod;
+				}
+				// 그 외는 현재 자릿값의 -1, +1 가능
+				else {
+					dp[i][j] = (dp[i-1][j-1]+ dp[i-1][j+1])%mod;
+				}
 			}
-			
 		}
-		int sum = 0;
-		for(int i=0 ; i < 10 ; i++) {
-			System.out.println(dp[N][i]);
+		long sum = 0;
+		for(int i = 0 ; i < 10 ; i++) {
 			sum += dp[N][i];
 		}
-		System.out.println(sum);
+		System.out.println(sum%mod);
 		
 		
 	}

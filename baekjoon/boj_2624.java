a1d0f303a30773c4c539eb2ed86579cad291f6b9
baeseoeun <baeseoeun1221@gmail.com>
2023-08-02 17:33:40 +0900

    동전바꿔주기 - 진행중

--- baekjoon/boj_2624.java
+++ baekjoon/boj_2624.java
@@ -8,35 +8,39 @@ public class boj_2624 {
 	static BufferedReader br;
 	static StringTokenizer st;
 	static int[][] dp;
-	static ArrayList<int[]> coins = new ArrayList<>();
+	static Coin[] coins;
+	
+	
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 		int n = Integer.parseInt(br.readLine());
 		int m = Integer.parseInt(br.readLine()); //동전가짓수
-	
+		coins = new Coin[m];
 		dp = new int[m+1][n+1];
 		for(int i=1 ; i <= n  ; i++) {
 			dp[1][i] = 1;
-			for(int[] each : coins) {
-				if(i % each[0] == 0) dp[1][i] += 1;
-			}
+			
 		}
 		
 		
 		for(int i=0 ; i < m ; i++) {
 			st = new StringTokenizer(br.readLine());
-			int[] coin = new int[2];
 			
-			coin[0] = Integer.parseInt(st.nextToken());
-			coin[1] = Integer.parseInt(st.nextToken());
-			coins.add(coin);
+			int price = Integer.parseInt(st.nextToken());
+			int cnt = Integer.parseInt(st.nextToken());
+			Coin coin  = new Coin(price , cnt);
+			coins[i]=coin;
 		}
 		
-//		for(int i = 2 ; i <= m ; i++) {
-//			for(int j = 1; j <= n ;j++) {
-//				
-//				dp[i][j] = dp[i-1][j];
-//			}
-//		}
-	}	
+	}
+	
+	
+	public static class Coin{
+		int price;
+		int cnt;
+		Coin(int price, int cnt){
+			this.price = price;
+			this.cnt = cnt;
+		}
+	}
 }

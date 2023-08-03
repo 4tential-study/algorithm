c74ece95085ae1c9573ba01334c1b43c5e360b77
baeseoeun <baeseoeun1221@gmail.com>
2023-08-03 15:04:39 +0900

    [Baekjoon] 동전1 - dp

--- baekjoon/boj_2293.java
+++ baekjoon/boj_2293.java
@@ -1,42 +1,33 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.HashSet;
 import java.util.StringTokenizer;
 
 public class boj_2293 {
-    static StringTokenizer st;
-    static int[] dp;
-    static int n;
-    static int k;
-    static HashSet<Integer> set  = new HashSet<>();
-    static int[] coins;
-
+	
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String[] strings = br.readLine().split(" ");
-        n = Integer.parseInt(strings[0]);
-        k = Integer.parseInt(strings[1]);
-        coins = new int[n];
-        dp = new int[k+1];
-        for(int i=0 ; i < n ; i++){
-            Integer coin = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int n = Integer.parseInt(st.nextToken()); //3동전개수
+        int k = Integer.parseInt(st.nextToken());//10합
+        int[] coins = new int[101];
+        int[] dp = new int[10001];
+        Integer coin;
+        for(int i=1 ; i <= n ; i++){
+            coin = Integer.parseInt(br.readLine());
             coins[i] = coin;
-            dp[coin] += 1;
         }
-
-        for(int i=1 ; i <= k ; i++){
-            set.clear();
-            for(int j=0 ; j < n ; j++){
-                if (set.contains(i-coins[j])) continue;
-                if (i > coins[j]) {
-//                    if (i % coins[j] == 0 ) continue;
-                    dp[i] += dp[i - coins[j]];
-                    set.add(coins[j]);
-                }
+        
+        dp[0] = 1;
+      
+        for(int i=1 ; i <= n ; i++){
+        	for(int j=coins[i] ; j <= k ; j++){ 
+                	dp[j] += dp[j - coins[i]]; 
             }
         }
-
         System.out.println(dp[k]);
+        }
+
+        
     }
-}
+

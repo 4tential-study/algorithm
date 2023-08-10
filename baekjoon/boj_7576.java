550bfcd6ec0cb866f4be1566bc518180ca89208d
seoeunbae <enyoupark@gmail.com>
2023-08-10 21:30:30 +0900

    토마토 - 진행중2

--- baekjoon/boj_7576.java
+++ baekjoon/boj_7576.java
@@ -7,23 +7,30 @@ import java.util.StringTokenizer;
 public class boj_7576 {
     static BufferedReader br;
     static int[][] board;
+    static boolean[][] visited;
 
     static StringTokenizer st;
+    static int x;
+    static int y;
 
-
+    static int[] dy  = new int[]{0,-1,0,1};
+    static int[] dx = new int[]{-1,0,-1,0};
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        int x = Integer.parseInt(s[0]);
-        int y = Integer.parseInt(s[1]);
-
+        x = Integer.parseInt(s[0]);
+        y = Integer.parseInt(s[1]);
+        ArrayList<int[]> start = new ArrayList<>();
         board = new int[y][x];
 
         for(int i=0 ; i < y ; i++){
             st = new StringTokenizer(br.readLine());
             for(int j=0 ; j < x ; j++){
                 board[i][j]= Integer.parseInt(st.nextToken());
+                if(board[i][j]== 1) {
+                    start.add(new int[]{i,j});
+                }
             }
         }
 
@@ -39,11 +46,28 @@ public class boj_7576 {
             return;
         }
 
-        bfs(new int[]{0,0});
+        bfs(start);
 
     }
 
-    public static void bfs(int[] start){
-        Queue<int[]> queue = new Queue<>();
+    public static void bfs(ArrayList<int[]> start){
+        Queue<int[]> queue = new LinkedList<>();
+        for(int[] each : start){
+            queue.add(each);
+            visited[each[0]][each[1]] = true;
+        }
+        while(!queue.isEmpty()){
+            int[] poll = queue.poll();
+            for(int i=0 ; i < 4 ; i++) {
+                int ay = poll[0] + dy[i];
+                int ax = poll[1] + dx[i];
+                if( ax>=0 && ax < x && ay >=0 && ay < y && board[ay][ax]==0 &&!visited[ay][ax]){
+                    queue.add(new int[]{ay,ax});
+                    visited[ay][ax] = true;
+                    board[ay][ax] = 1;
+                }
+            }
+        }
+
     }
 }

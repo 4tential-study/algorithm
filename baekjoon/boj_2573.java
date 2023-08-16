3063348d83a0bb138265fe81951411189cdccde3
baeseoeun <baeseoeun1221@gmail.com>
2023-08-16 09:32:04 +0900

    [Baekjoon] 빙산 - dfs/bfs

--- baekjoon/boj_2573.java
+++ baekjoon/boj_2573.java
@@ -20,17 +20,18 @@ public class boj_2573 {
 
     static final int SEA = -10;
     static int cnt = 0;
-    static int MELT = 0;
+    static final int MELT = 0;
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
         String[] s = br.readLine().split(" ");
-        n = Integer.parseInt(s[0]); //y
-        m = Integer.parseInt(s[1]); //x
+        n = Integer.parseInt(s[0]); 
+        m = Integer.parseInt(s[1]); 
         years = new int[n][m];
         map = new int[n][m];
         visited = new boolean[n][m];
 
+        //초기화 하면서, 녹은 바다(MELT)와 원래 바다(SEA)를 구분하기 위해 다른 파이널 변수를 사용
         for(int i=0; i < n ; i++){
             st  = new StringTokenizer(br.readLine());
             for(int j=0 ; j < m ; j++){
@@ -40,48 +41,49 @@ public class boj_2573 {
                 }
             }
         }
-
+        
+        //dfs수행 횟수가 2일 경우 멈춤
         while(cnt < 2) {
             year++;
+            //매년 남은 빙산을 큐에 담는다
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if(map[i][j] > 0) dq.offer(new int[]{i,j});
                 }
             }
-            System.out.println(cnt+ " " + "bfs");
             bfs();
+            //녹은 바다를 다음해 수행전에 원래바다처럼 변경해준다.
+            //두 바다를 퉁치면, bfs수행중인 같은해에도 빙산에 영향을 줘버린다. 
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
-                    System.out.print(map[i][j] + " ");
-                }
-                System.out.println();
+                    if(map[i][j] == MELT) map[i][j] = SEA;
+                }             
             }
-            int ice = 0;
+            
+            int ice = 0;            
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (map[i][j] != SEA && map[i][j] != MELT) ice++;
                 }
             }
-            System.out.println(ice);
+                        
             if (ice == 0) {
                 System.out.println(0);
                 return;
             }
-
+            
             visited = new boolean[n][m];
+            cnt = 0;
             for (int i = 0; i < n; i++) {
                 for (int j = 0; j < m; j++) {
                     if (map[i][j] > 0 && !visited[i][j]) {
-                        System.out.println(i + " " + j);
-                        System.out.println("??");
                         dfs(i, j);
-                        cnt++;
+                        cnt++; //dfs수행횟수==덩어리 개수
                     }
                 }
             }
-            System.out.println("cmt "+cnt);
         }
-        System.out.println("year "+year);
+        System.out.println(year);
 
     }
 
@@ -91,8 +93,7 @@ public class boj_2573 {
             for(int f=0 ; f < 4; f++){
                 int y = poll[0] + delta[f][0];
                 int x = poll[1] + delta[f][1];
-                if(y >= 0 && y < n && x >= 0 && x < m && map[y][x] <= 0 ){
-//                    dq.offer(new int[]{y,x});
+                if(y >= 0 && y < n && x >= 0 && x < m && map[y][x] == SEA ){
                     map[poll[0]][poll[1]]--;
                     years[y][x] = years[poll[0]][poll[1]] + 1;
                     map[poll[0]][poll[1]] = Math.max(map[poll[0]][poll[1]], 0);
@@ -115,9 +116,7 @@ public class boj_2573 {
                 if(ay >= 0 && ay < n && ax >= 0 && ax < m && map[ay][ax] > 0 && !visited[ay][ax]){
                     stack.add(new int[]{ay,ax});
                     visited[ay][ax] = true;
-
                 }
-
             }
         }
     }

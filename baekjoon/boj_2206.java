cc53af349b6a30a22873fc579c471dfe78748556
seoeunbae <enyoupark@gmail.com>
2023-08-21 20:21:02 +0900

    [Baekjoon] 2931.가스관 - 구현

--- baekjoon/boj_2206.java
+++ baekjoon/boj_2206.java
@@ -12,10 +12,11 @@ public class boj_2206 {
     static int[][] map;
     static int n;
     static int m;
-    static int broken;
     static int[][] ans ;
+    static boolean[][] used;
+
     static final int VISITED  = 100;
-    static int[][] deltas = {{-1,0}, {0,-1}, {0,1}, {1,0}};
+    static int[][] deltas = {{1,0},{0,1}, {-1,0}, {0,-1}};
 
     public static void main(String[] args) throws IOException {
         br = new BufferedReader(new InputStreamReader(System.in));
@@ -25,17 +26,31 @@ public class boj_2206 {
         m = Integer.parseInt(str[1]);
         map = new int[n][m];
         ans = new int[n][m];
+        used= new boolean[n][m];
 
         for(int i=0 ; i < n ; i++){
-            st = new StringTokenizer(br.readLine());
+            String s = br.readLine();
             for(int j = 0  ; j < m ; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = s.charAt(j) - '0';
 
             }
         }
-
+        ans[0][0] = 1;
         bfs(0,0);
-        System.out.println(map[n-1][m-1]);
+        for(int i=0 ; i < n  ; i++){
+            for(int j = 0 ; j < m ;j ++){
+                System.out.print(ans[i][j]);
+            }
+            System.out.println();
+        }
+        for(int i=0 ; i < n  ; i++){
+            for(int j = 0 ; j < m ;j ++){
+                System.out.print(used[i][j]);
+            }
+            System.out.println();
+        }
+        int answer = ans[n-1][m-1] != 0 ? ans[n-1][m-1] : -1;
+        System.out.println(answer);
     }
 
     public static void bfs(int y, int x){
@@ -43,21 +58,22 @@ public class boj_2206 {
         dq.add(new int[]{y,x});
 
         while(!dq.isEmpty()){
-            int[] poll = dq.poll();
+            int[] poll = dq.poll(); //5,2
             for(int i=0 ; i < 4 ; i++){
                 int ay = poll[0] + deltas[i][0];
                 int ax = poll[1] + deltas[i][1];
 
                 if(ay >= 0 && ay < n && ax >= 0 && ax < m && map[ay][ax] != VISITED){
-                    if(broken == 0 && map[ay][ax] == 1 ){
+                    if(!used[poll[0]][poll[1]] && map[ay][ax] == 1 ){
                         map[ay][ax] = VISITED;
-                        broken += 1;
+                        used[ay][ax] = true;
                         dq.add(new int[]{ay,ax});
                         ans[ay][ax] = ans[poll[0]][poll[1]] + 1;
                     } else if(map[ay][ax] == 0) {
                         map[ay][ax] = VISITED;
                         dq.add(new int[]{ay, ax});
                         ans[ay][ax] = ans[poll[0]][poll[1]] + 1;
+                        used[ay][ax] = used[poll[0]][poll[1]];
                     }
                 }
             }

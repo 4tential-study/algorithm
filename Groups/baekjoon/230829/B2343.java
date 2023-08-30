50bdc27c2eb65b0e0e1ec188e39c3fdbba8501a1
parkdamin <ferrorist@naver.com>
2023-08-30 10:39:48 +0900

    230831 충돌 해결 중

--- Groups/baekjoon/230829/B2343.java
+++ Groups/baekjoon/230829/B2343.java
@@ -4,18 +4,22 @@ import java.io.InputStreamReader;
 public class B2343 {
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
   private static int[] classes;
 =======
 >>>>>>> 6caf306 (230829 푸는 중...)
 =======
   private static int[] classes;
 >>>>>>> d68d8db ([Baekjoon] 2343, 2467)
+=======
+>>>>>>> 1690e96 (230829 푸는 중...)
   public static void main(String[] args) throws Exception {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
     String[] input = in.readLine().split(" ");
     int class_count = Integer.parseInt(input[0]);
     int blueray_count = Integer.parseInt(input[1]);
 
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     classes = new int[class_count];
@@ -94,5 +98,26 @@ public class B2343 {
 =======
     return cnt;
 >>>>>>> d68d8db ([Baekjoon] 2343, 2467)
+=======
+    int max_classTime = 0;
+    int[] classDP = new int[class_count+1];
+
+    input = in.readLine().split(" ");
+    for(int i = 0; i < class_count; i++){
+      int value = Integer.parseInt(input[i]);
+      max_classTime = Math.max(value, max_classTime);
+      classDP[i+1] = classDP[i] + value;
+    }
+    if(blueray_count == 1){
+      System.out.println(classDP[class_count]);
+      return;
+    }
+    else if(blueray_count == class_count){
+      System.out.println(max_classTime);
+      return;
+    }
+
+      
+>>>>>>> 1690e96 (230829 푸는 중...)
   }
 }
--- Groups/baekjoon/230829/B2467.java
+++ Groups/baekjoon/230829/B2467.java
@@ -2,27 +2,34 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 import java.util.Arrays;
 >>>>>>> 6caf306 (230829 푸는 중...)
 =======
 >>>>>>> d68d8db ([Baekjoon] 2343, 2467)
+=======
+>>>>>>> 1690e96 (230829 푸는 중...)
 
 public class B2467 {
   public static void main(String[] args) throws Exception {
     BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
     StringBuilder sb = new StringBuilder();
 =======
 >>>>>>> 6caf306 (230829 푸는 중...)
 =======
     StringBuilder sb = new StringBuilder();
 >>>>>>> d68d8db ([Baekjoon] 2343, 2467)
+=======
+>>>>>>> 1690e96 (230829 푸는 중...)
     int Integer_count = Integer.parseInt(in.readLine()); // 전체 용액의 수
     int[] values = new int[Integer_count];
 
     String[] input = in.readLine().split(" ");
+<<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
     for(int i = 0; i < Integer_count; i++)  values[i] = Integer.parseInt(input[i]);
@@ -88,3 +95,9 @@ public class B2467 {
 =======
 }
 >>>>>>> d68d8db ([Baekjoon] 2343, 2467)
+=======
+    for(int i = 0; i < Integer_count; i++)  values[i] = Integer.parseInt(input[i]);
+    
+  }  
+}
+>>>>>>> 1690e96 (230829 푸는 중...)

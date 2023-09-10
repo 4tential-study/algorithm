2d85fc505e01bb123e75ad6a561bd4a23d8b25bd
seoeunbae <enyoupark@gmail.com>
2023-09-11 07:51:27 +0900

    [baekjoon] 전화번호 목록

--- baekjoon/boj_5052.java
+++ baekjoon/boj_5052.java
@@ -1,71 +1,58 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
 import java.util.Map;
 
 public class boj_5052 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
-	}
-	public class TrieNode {
-
-		// [ 변수 ]
-		// 자식 노드 맵
-		private Map<Character, TrieNode> childNodes = new HashMap<>();
-		// 마지막 글자인지 여부
-		private boolean isLastChar;
-		
-		// [ GETTER / SETTER 메서드 ]
-		// 자식 노드 맵 Getter
-		Map<Character, TrieNode> getChildNodes() {
-			return this.childNodes;
-		}
-		// 마지막 글자인지 여부 Getter
-		boolean isLastChar() {
-			return this.isLastChar;
-		}
-		// 마지막 글자인지 여부 Setter
-		void setIsLastChar(boolean isLastChar) {
-			this.isLastChar = isLastChar;
-		}
-		
-	}
-	
-	public class Trie {
-
-		// [ 변수 ]
-		// 루트 노드
-		private TrieNode rootNode;
-
-		// [ 생성자 ]
-		Trie() {
-			rootNode = new TrieNode();
-		}
-		
-		void insert(String word) {
-			TrieNode thisNode = this.rootNode;
-
-			for (int i = 0; i < word.length(); i++) {
-				thisNode = thisNode.getChildNodes().computeIfAbsent(word.charAt(i), c -> new TrieNode());
-			}	
-			thisNode.setIsLastChar(true);
-		}
-	    
-		// 특정 단어가 들어있는지 확인
-		boolean contains(String word) {
-			TrieNode thisNode = this.rootNode;
-
-			for (int i = 0; i < word.length(); i++) {
-				char character = word.charAt(i);
-				TrieNode node = thisNode.getChildNodes().get(character);
-
-				if (node == null)
-					return false;
-
-				thisNode = node;
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		for(int i=0 ; i < T ; i++){
+			int n = Integer.parseInt(br.readLine());
+			Trie trie = new Trie();
+			String minS = "0000000000";
+			for(int j=0 ; j < n ; j++){
+				String s = br.readLine();
+				trie.insert(s);
+				if(minS.length() >= s.length()){
+					minS = s;
+				}
 			}
 
-			return thisNode.isLastChar();
+			if(trie.search(minS)==null) {
+				System.out.println("YES");
+			} else System.out.println("NO");
+
+		}
+	}
+
+	
+	public static class Node{
+		HashMap<Character, Node> child = new HashMap<>();
+		boolean isEnd;
+
+
+	}
+	public static class Trie{
+		Node root = new Node();
+
+		void insert(String str){
+			Node node = this.root;
+			for(char ch : str.toCharArray()){
+				node = node.child.computeIfAbsent(ch, key -> new Node());
+			}
+			node.isEnd = true;
+		}
+
+		Node search(String str){
+			Node node = this.root;
+			for(char ch : str.toCharArray()){
+				node = node.child.getOrDefault(ch, null);
+				if(node == null) return null;
+			}
+			return node;
 		}
 	}
 }

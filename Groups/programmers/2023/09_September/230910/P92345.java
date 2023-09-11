94979df36c4187f0b5e7c001d2f9f5c17648ad69
Ferrorist <34542316+Ferrorist@users.noreply.github.com>
2023-09-11 21:28:09 +0900

    [Programmers] 92345. 사라지는 발판 - 백트래킹

--- Groups/programmers/2023/09_September/230910/P92345.java
+++ Groups/programmers/2023/09_September/230910/P92345.java
@@ -1,70 +1,40 @@
-// 아직 푸는 중...
-// 문제의 size를 생각하면 완전 탐색으로도 풀릴 것 같다.
 class Solution {
-    // 상 하 좌 우
-    static final int[][] dir = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
-    static int answer = Integer.MAX_VALUE;
-    // 매개변수 : 
-    // 게임판( 1: 발판이 있음, 0: 발판이 없음 ), 
-    // 플레이어 A의 좌표, 플레이어 B의 좌표 (y, x)
-    public static int solution(int[][] board, int[] aloc, int[] bloc) {
-        Search(board, aloc, bloc, 0);
-        return answer;
+    private static final int dir[][] = {{-1, 0}, {1, 0}, {0, -1}, {0, 1}};
+    boolean[][] visited;
+    public int solution(int[][] board, int[] aloc, int[] bloc){
+        visited = new boolean[board.length][board[0].length];
+
+        return solve(board, aloc, bloc);
     }
 
-    static void Search(int[][] board, int[] current, int[] opponent, int count){
+    // 플레이어들이 최선을 다하였을 때의 이동 횟수
+    // 짝수 : 플레이어의 패배, 홀수 : 플레이어의 승리
+    public int solve(int[][] board, int[] current, int[] opponent){
         int cy = current[0], cx = current[1];
-        
-        // 현재 탐색을 시작한 위치에 발판이 없다면 탐색 중단.
-        if(board[cy][cx] == 0){
-            answer = Math.min(answer, count); return;
-        }
- 
-        // 주변을 탐색
-        int[] around_state = new int[4];
-        boolean none_player = false, canMove = false;
+        if(visited[cy][cx]) return 0;
+
+        int result = 0;
         for(int type = 0; type < 4; type++){
             int dy = cy + dir[type][0];
             int dx = cx + dir[type][1];
 
-            // 해당 위치로 갈 수 없다면 -1
-            if(!inBoard(board, dy, dx) || board[dy][dx] == 0) around_state[type] = -1;
+            if(!inBoard(board, dy, dx) || visited[dy][dx] || board[dy][dx] == 0) continue;
+            
+            visited[cy][cx] = true;
+            int value = solve(board, opponent, new int[] {dy, dx}) + 1;
+            visited[cy][cx] = false;
 
-            // 해당 위치에 상대 플레이어가 있다면 2
-            else if(dy == opponent[0] && dx == opponent[1]) {
-                canMove = true;
-                around_state[type] = 2;
-            }
-            // 아무도 없는 발판이라면 1
-            else {
-                canMove = true;
-                none_player = true;
-                around_state[type] = board[dy][dx];
-            }
+            if(result % 2 == 0 && value % 2 == 1) result = value;
+            else if(result % 2 == 0 && value % 2 == 0) result = Math.max(result, value);
+            else if(result % 2 == 1 && value % 2 == 1) result = Math.min(result, value);
         }
 
-        if(!canMove){
-            answer = Math.min(answer, count); return;
-        }
-        // 이동할 수 있는 발판 중 상대 플레이어가 없는 발판이 없다면,
-        // 이동할 수 있는 발판이 그 곳밖에 없다는 의미이므로 그 발판으로 이동한다.
-        else if(!none_player){
-            int[][] temp = copyBoard(board);
-            temp[cy][cx] = 0;  Search(temp, opponent, opponent, count+1);
-        }
-        // 이동할 수 있는 발판 중 상대 플레이어가 없는 발판이 있다면,
-        // 그 발판으로 우선 이동한다.
-        else {
-            for(int type = 0; type < 4; type++){
-                if(around_state[type] != 1) continue;
-                int dy = cy + dir[type][0];
-                int dx = cx + dir[type][1];
+        return result;
+    }
 
-                int[][] temp = copyBoard(board);
-                temp[cy][cx] = 0;
-                Search(temp, opponent, new int[] {dy, dx}, count+1);
-            }
-        }
+    static boolean inBoard(int[][] board, int y, int x) {
+        int length_y = board.length, length_x = board[0].length;
+        return (y >= 0 && x >= 0 && y < length_y && x < length_x);
     }
 
     static int[][] copyBoard(int [][] board){
@@ -75,15 +45,4 @@ class Solution {
 
         return temp;
     }
-
-    static boolean inBoard(int[][] board, int y, int x) {
-        int length_y = board.length, length_x = board[0].length;
-        return (y >= 0 && x >= 0 && y < length_y && x < length_x);
-    }
-
-    // public static void main(String[] args) {
-    //     int[][] board = {{1}};
-    //     int[] Aloc = {0, 0}, Bloc = {0, 0};
-    //     System.out.println(solution(board, Aloc, Bloc));
-    // }
 }

96f6f6f3ab974686dbe3d350bb83bf257794e4ef
ferrorist <ferrorist@naver.com>
2023-09-14 20:53:39 +0900

    [Programmers] 67259. 경주로 건설 - DFS, 백트래킹

--- Groups/programmers/2023/09_September/230914/P67259.java
+++ Groups/programmers/2023/09_September/230914/P67259.java
@@ -1,37 +1,40 @@
-// DFS + 백트래킹 -> 시간초과
+import java.util.Arrays;
+
 class Solution {
-    // 직선도로 100원, 코너 : +500원
-    private static final int ROAD = 100, CORNER = 500;
     // 우 하 좌 상
-    // 우(0) + 좌(2) -> 짝수, 상(3) + 하(1) -> 짝수
-    // 우(0) + 상(3) -> 홀수
     private static final int[][] dir = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
     private static int min_cost = Integer.MAX_VALUE; // 최소 비용
-    private static int[] finish;
+    private static int[] finish; // 도착지
+    private static int[][] weights; // 각 칸에 도달할 수 있는 최소 비용
     public static int solution(int[][] board) {
         finish = new int[] {board.length-1 , board[0].length-1};
+        weights = new int[board.length][board[0].length];
+        for(int i = 0; i < weights.length; i++) Arrays.fill(weights[i], Integer.MAX_VALUE);
 
         boolean[][] visited = new boolean[board.length][board[0].length];
+        
         visited[0][0] = true;
-        for(int type = 0; type < 4; type++){
-            int dy = dir[type][0];
-            int dx = dir[type][1];
+        weights[0][0] = 0;
 
-            if(inBoard(board, dy, dx) && board[dy][dx] == 0){
+        for(int type = 0; type < 2; type++){
+            int dy = dir[type][0], dx = dir[type][1];
+            if(board[dy][dx] == 0){
                 visited[dy][dx] = true;
-                solve(board, new int[] {dy, dx}, type, ROAD, 0, visited);
+                solve(board, new int[] {dy, dx}, type, 100, visited);
                 visited[dy][dx] = false;
             }
         }
         return min_cost;
     }
 
-    public static void solve(int[][] board, int[] current, int cur_dir, int cost, int corner_count, boolean[][] visited){
-        if(min_cost < cost) return;
+    public static void solve(int[][] board, int[] current, int cur_dir, int cost, boolean[][] visited){
         int cy = current[0], cx = current[1];
+         // 각 칸에 도달할 수 있는 최소 비용보다 현재 비용이 높을 경우 return.
+        if(weights[cy][cx] < cost) return;
+        else if(weights[cy][cx] > cost) weights[cy][cx] = cost;
+        
         // 목적지에 도착하였으면, 최소 비용 저장
         if(cy == finish[0] && cx == finish[1]){
-            // printArray(board, visited); System.out.println("cost : " + cost + ", corner_count : " + corner_count);
             min_cost = Math.min(min_cost, cost);
             return;
         }
@@ -42,48 +45,19 @@ class Solution {
 
             if(inBoard(board, dy, dx) && board[dy][dx] == 0 && !visited[dy][dx]){
                 visited[dy][dx] = true;
-                int newCost = cost + ROAD;
-                if((cur_dir + type) % 2 == 1) 
-                    solve(board, new int[] {dy, dx}, type, newCost + CORNER, corner_count+1, visited);
+                if(cur_dir != type) // 현재 이동하는 경로와 다른 경우엔 커브를 돌아야 하므로 cost에 600(100+500) 추가
+                    solve(board, new int[] {dy, dx}, type, cost + 600, visited);
                 
-                else solve(board, new int[] {dy, dx}, type, newCost, corner_count, visited);
+                    // 그렇지 않다면 cost에 100 추가
+                else solve(board, new int[] {dy, dx}, type, cost + 100, visited);
                 visited[dy][dx] = false;
             }
         }
     }
-
-
-    // public static void main(String[] args) {
-    //     int[][] board = {
-    //         {0, 0, 0, 0, 0, 0, 0, 1}, 
-    //         {0, 0, 0, 0, 0, 0, 0, 0}, 
-    //         {0, 0, 0, 0, 0, 1, 0, 0}, 
-    //         {0, 0, 0, 0, 1, 0, 0, 0}, 
-    //         {0, 0, 0, 1, 0, 0, 0, 1}, 
-    //         {0, 0, 1, 0, 0, 0, 1, 0},
-    //         {0, 1, 0, 0, 0, 1, 0, 0}, 
-    //         {1, 0, 0, 0, 0, 0, 0, 0}
-    //     };
-    //     System.out.println(solution(board));
-    // }
 
     public static boolean inBoard(int[][] board, int y, int x){
         int length_y = board.length, length_x = board[0].length;
 
         return !(y < 0 || x < 0 || y >= length_y || x >= length_x);
     }
-
-    // static void printArray(int[][] board, boolean[][] visited){
-    //     StringBuilder sb = new StringBuilder();
-    //     for(int i = 0; i < visited.length; i++){
-    //         for(int j = 0; j < visited[0].length; j++){
-    //             if(board[i][j] == 1) sb.append("2 ");
-    //             else if(visited[i][j]) sb.append("1 ");
-    //             else sb.append("0 ");
-    //         }
-    //         sb.append("\n");
-    //     }
-
-    //     System.out.println(sb.toString());
-    // }
 }

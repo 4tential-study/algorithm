e0b9e71e8a2fdb392ac3afa131e8ff13a5f8fb35
ferrorist <ferrorist@naver.com>
2023-10-04 23:31:02 +0900

    09/23 ~ 09/26 복구

---
+++ Groups/baekjoon/2023/09_September/230923/B14719.java
@@ -0,0 +1,41 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B14719 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] input = in.readLine().split(" ");
+		int width = Integer.parseInt(input[1]);
+		
+		int[] heights = new int[width];
+		
+		input = in.readLine().split(" ");
+		int max_heights = Integer.MIN_VALUE, max_index = 0;
+		for(int i = 0; i < width; i++) {
+			heights[i] = Integer.parseInt(input[i]);
+			if(max_heights < heights[i]) {
+				max_heights = heights[i];	max_index = i;
+			}
+		}
+		int answer = 0;
+		for(int h = max_heights; h > 0; h--) {
+			int search = h, search_index = max_index;
+			for(int left = max_index-1; left >= 0; left--) {
+				if(heights[left] >= search) {
+					answer += (Math.abs(search_index - left) - 1);
+					search_index = left;
+				}
+			}
+			search_index = max_index;
+			for(int right = max_index+1; right < width; right++) {
+				if(heights[right] >= search) {
+					answer += (Math.abs(right - search_index) - 1);
+					search_index = right;
+				}
+			}
+		}
+		
+		System.out.println(answer);
+	}
+}
---
+++ Groups/baekjoon/2023/09_September/230926/B2156.java
@@ -0,0 +1,29 @@
+﻿import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class B2156 {
+	public static void main(String[] args) throws Exception {
+		BufferedReader in = new BufferedReader(new InputStreamReader(System.in));
+		int wine_count = Integer.parseInt(in.readLine());
+		int[] wines = new int[wine_count+1];
+		
+		for(int i = 1; i <= wine_count; i++)
+			wines[i] = Integer.parseInt(in.readLine());
+		
+		// 포도주
+		int[][] DP = new int[wine_count+1][3];
+		
+		// DP[i][n] = i번째 포도주일 때 연속으로 마신 n잔. (i >= 1)
+		DP[0][0] = DP[0][1] = DP[0][2] = 0;
+		
+		for(int i = 1; i <= wine_count; i++) {
+			DP[i][0] = Math.max(DP[i-1][0], Math.max(DP[i-1][1], DP[i-1][2]));
+			DP[i][1] = DP[i-1][0] + wines[i];
+			if(i >= 2)	DP[i][2] = DP[i-1][1] + wines[i];
+		}
+		
+		int answer = Math.max(DP[wine_count][0], Math.max(DP[wine_count][1], DP[wine_count][2]));
+		
+		System.out.println(answer);
+	}
+}

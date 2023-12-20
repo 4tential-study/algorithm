110d5ae06223b8e6b93bad2a2dea701988ea5079
object1997428 <71408968+object1997428@users.noreply.github.com>
2023-12-20 20:14:03 +0900

    fix

--- programmers/pgm-43163.cpp
+++ programmers/pgm-43163.cpp
@@ -1,70 +1,51 @@
-﻿#include <iostream>
-#include <string.h>
-#include <algorithm>
-#include <climits>
-#include <cmath>
-#include <cassert>
+﻿#include <string>
 #include <vector>
-#define MAX 987654321
+#include <algorithm>
+#include <iostream>
+#include <string.h>
+#include <climits>
+#include <cassert>
+#include <cmath>
+#include <queue>
+#include <unordered_map>
 #pragma warning(disable:4996)
 using namespace std;
-typedef long long int ll;
-typedef pair<int, int> pii;
-#define endl '\n'
-vector<int> arr1, arr2;//작은애, 큰애
-vector<ll> sum1, sum2; //누적합 저장
-int cnt1, cnt2; //처음 큐의 사이즈
+typedef pair<string, int> pii;
+unordered_map<string, int> arr;
+unordered_map<string, int> checked;
 
 
-int solution(vector<int> queue1, vector<int> queue2) {
 
-    // 1. 세팅
-    cnt1 = queue1.size(), cnt2 = queue2.size(); //처음 각 큐의 사이즈
-    ll sumv1 = 0, sumv2 = 0; //처음 각 큐의 합
-    for (int i = 0; i < queue1.size(); i++) sumv1 += queue1[i];
-    for (int i = 0; i < queue2.size(); i++) sumv2 += queue2[i];
+int bfs(string src, string dest) {
+    queue<pii> q;
+    q.push({ src,0 });
+    checked[src] = 1;
+    while (!q.empty()) {
+        string cur = q.front().first;
+        int cnt = q.front().second;
+        q.pop();
 
-    //큰애에서 빼줘야 하는 값 need 구하기
-    //그냥 sum2-평균값
-    ll sum = sumv1 + sumv2;
-    if (sum % 2 == 1) return -1; //합이 홀수라 처음부터 불가능한 케이스(이런테케 없었음..)
-    ll half = sum /= 2;
-    ll need = max(sumv1, sumv2) - half;
-    // cout<<need<<endl;
+        if (cur == dest) return cnt; //1부터 시작했으니까 cnt+1-1
 
-    if (sumv1 == sumv2) return 0; //처음부터 정답인 케이스
-    //arr1에 작은애, arr2에 큰애 들어가게
-    if (sumv1 > sumv2) arr1 = queue2, arr2 = queue1;
-    else arr1 = queue1, arr2 = queue2;
+        for (int i = 0; i < cur.length(); i++) {
+            for (int j = 0; j < 26; j++) {
+                string next = cur; next[i] = 'a' + j;
+                if (arr[next] == 1 && checked[next] == 0) {
+                    checked[next] = 1;
+                    q.push({ next,cnt + 1 });
+                }
+            }
+        }
 
-    //arr1은 기존arr1 + arr2 를 가질거임
-    for (int i = 0; i < arr2.size(); i++) arr1.push_back(arr2[i]);
-
-    //2. 누적합 구하기
-    sum1.resize(arr1.size() + 1, 0);
-    sum2.resize(arr2.size() + 1, 0);
-
-    sum1[0] = 0;
-    sum2[0] = 0;
-    for (int i = 0; i < arr1.size(); i++) {
-        sum1[i + 1] = sum1[i] + arr1[i];
     }
-    for (int i = 0; i < arr2.size(); i++) {
-        sum2[i + 1] = sum2[i] + arr2[i];
-    }
-
-    //3. 투포인터
-    // sum2[start]-sum1[end]이 need보다 작으면 start++, 크면 end++해서
-    // sum2[start]-sum1[end]=need읜 start, end를 구함
-    int start = 0, end = 0;
-    while (start < arr2.size()) {
-        int cur = sum2[start] - sum1[end];
-        if (end == start + cnt1 || need > cur) start++;
-        else if (need == cur) break;
-        else end++;
-    }
-
-    if (start >= arr2.size()) return -1;
-    return start + end;
+    return 0;
 }
 
+int solution(string begin, string target, vector<string> words) {
+    for (auto word : words) {
+        arr[word] = 1;
+    }
+
+    int answer = bfs(begin, target);
+    return answer;
+}
